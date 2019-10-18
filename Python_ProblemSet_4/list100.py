f706d3372ec73a1f9bb5edc2d51f103c78d9337d
info <info@info08.cshl.edu>
2019-10-17 22:01:59 -0400

    still need to solve factorial problem

---
+++ Python_ProblemSet_4/list100.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+
+
+#calculating the factorial of 1000
+factorial = []
+count = 1001
+while count > 0:
+	x = count
+	y = count-1
+	product = (x)*(y)
+print(product)
+
+
+
